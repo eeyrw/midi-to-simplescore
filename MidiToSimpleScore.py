360a799b95c82a6a57c553f5ab762593af807ee2
yuan <ayrw@163.com>
2021-10-02 16:05:12 +0800

    M

--- MidiToSimpleScore.py
+++ MidiToSimpleScore.py
@@ -6,6 +6,10 @@ from functools import reduce
 from io import StringIO
 import os
 from terminaltables import AsciiTable
+import argparse
+from gooey import Gooey
+from gooey import GooeyParser
+from string import Template
 
 
 def readMidiFile(filePath):
@@ -49,16 +53,16 @@ def getNoteNameValueMap():
     return noteNameValueMap
 
 
-def calcTranspose(centroidNote, lowestNote, highestNote, centerOfSuggest, lowerBoundPitch, upperBoundPitch):
-    wantedTranspose = centerOfSuggest - centroidNote
+def calcTranspose(centroidNote, lowestNote, highestNote, voiceCenterNote, lowerBoundNote, upperBoundNote):
+    wantedTranspose = voiceCenterNote - centroidNote
 
     wantedHighestNote = highestNote + wantedTranspose
     wantedLowestNote = lowestNote + wantedTranspose
 
     suggestTranpose = wantedTranspose
 
-    offestToValidHighestNote = upperBoundPitch - wantedHighestNote
-    offestToValidLowestNote = lowerBoundPitch - wantedLowestNote
+    offestToValidHighestNote = upperBoundNote - wantedHighestNote
+    offestToValidLowestNote = lowerBoundNote - wantedLowestNote
 
     if offestToValidHighestNote >= 0 and offestToValidLowestNote <= 0:
         suggestTranpose += 0
@@ -75,14 +79,13 @@ def calcTranspose(centroidNote, lowestNote, highestNote, centerOfSuggest, lowerB
                  ('lowestNote', str(lowestNote)),
                  ('highestNote', str(highestNote)),
                  ('centroidNote', str(centroidNote)),
-                 ('centerOfSuggest', str(centerOfSuggest)),
+                 ('voiceCenterNote', str(voiceCenterNote)),
                  ('suggestTranpose', str(suggestTranpose)),
                  ]
     table = AsciiTable(tableData)
     table.inner_row_border = True
     table.title = 'Transpose'
-    print(table.table)
-    return suggestTranpose
+    return suggestTranpose, table.table
 
 
 def generateNoteOnSetList(noteOnList):
@@ -142,12 +145,82 @@ def generateDeltaBin(noteOnSetList, tickPerSecond, transpose=0):
     return mem
 
 
-noteNameValueMap = getNoteNameValueMap()
-filePath = r"C:\Users\yuan\Desktop\midi-to-hex-example\midi-sample\piano_sonata_457_1_(c)oguri.mid"
-noteOnList = readMidiFile(filePath)
-centroidNote, lowestNote, highestNote = analyzeNoteList(noteOnList)
-t = calcTranspose(centroidNote, lowestNote,
-                  highestNote,noteNameValueMap['C:4'] , 0, 127)
-noteOnSetList = generateNoteOnSetList(noteOnList)
-bin = generateDeltaBin(noteOnSetList, 125, t)
-print(getCStyleSampleDataString(bin, 16, '// %s' % os.path.basename(filePath)))
+def formatFileByParam(templateFile, outputFile, param):
+    with open(templateFile, 'r') as tmplFile:
+        tmplString = tmplFile.read()
+        s = Template(tmplString)
+        with open(outputFile, 'w') as outFile:
+            outFile.write(s.safe_substitute(param))
+
+
+def genCode(templateFiles, scoreBytes, scoreMetaInfo, outputDir):
+    scoreBytesDataString = getCStyleSampleDataString(
+        scoreBytes, 16, dataDescription='')
+    paramDict = {}
+    paramDict['ScoreDataLen'] = len(scoreBytes)
+    paramDict['ScoreMetaInfo'] = scoreMetaInfo
+    paramDict['ScoreData'] = scoreBytesDataString
+
+    for templateFile in templateFiles:
+        formatFileByParam(templateFile, os.path.join(outputDir, os.path.basename(os.path.splitext(
+            templateFile)[0])), paramDict)
+
+
+@Gooey
+def main():
+    noteNameValueMap = getNoteNameValueMap()
+    parser = GooeyParser(
+        description='The midi to simple score converting tool.')
+    # parser = argparse.ArgumentParser(
+    #     description='The midi to simple score converting tool.')
+    parser.add_argument('--midi', type=str, default=r"C:\Users\yuan\Desktop\midi-to-hex-example\midi-sample\Mozart_314_Allegro.mid",
+                        help='midi file path.', widget='FileChooser')
+    parser.add_argument('--useExtraTranspose', default=False, action='store_true',
+                        help='Use user specific tranpose value by param transpose.')
+    parser.add_argument('--transpose', type=int, default=0,
+                        help='Transpose in half note.')
+    parser.add_argument('--upperBoundNote', type=int, default=127,
+                        help='Max midi note which target device can support.')
+    parser.add_argument('--lowerBoundNote', type=int, default=0,
+                        help='Min midi note which target device can support.')
+    parser.add_argument('--voiceCenterNote', type=int, default=noteNameValueMap['C:4'],
+                        help='Center note of target voice.')
+    parser.add_argument('--tickPerSecond', type=int, default=125,
+                        help='Ticks per second on target device.')
+    parser.add_argument('--outputDir', type=str, default='.',
+                        help='Output directory.', widget='DirChooser')
+    parser.add_argument('--template', type=str, default='8051_sdcc',
+                        help='Using interal template by specifing type.')
+    parser.add_argument('--extraTemplate', nargs='+', type=str, default=[],
+                        help='Using extra template files instead of self-contained template.')
+    args = parser.parse_args()
+
+    if args.template != None:
+        templateFileList = []
+        for filePath in os.listdir(os.path.join('./template', args.template)):
+            if os.path.splitext(filePath)[1] == '.template':
+                templateFileList.append(os.path.join(
+                    './template', args.template, filePath))
+    else:
+        templateFileList = args.extraTemplate
+
+    filePath = args.midi
+    noteOnList = readMidiFile(filePath)
+    centroidNote, lowestNote, highestNote = analyzeNoteList(noteOnList)
+    if not args.useExtraTranspose:
+        t, transposeMetaInfo = calcTranspose(centroidNote, lowestNote,
+                                             highestNote, args.voiceCenterNote, args.lowerBoundNote, args.upperBoundNote)
+    else:
+        t = args.transpose
+        transposeMetaInfo = 'Use Extern Transpose: %d' % t
+    noteOnSetList = generateNoteOnSetList(noteOnList)
+    bin = generateDeltaBin(noteOnSetList, args.tickPerSecond, t)
+
+    scoreMetaInfo = 'File: %s\n' % os.path.basename(filePath)
+    scoreMetaInfo += transposeMetaInfo
+
+    genCode(templateFileList, bin, scoreMetaInfo, args.outputDir)
+
+
+if __name__ == "__main__":
+    main()
